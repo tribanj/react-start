4776663448a53deaa653043a6c44f1a535873f13
Tribhuwan <tribhuwanja@gmail.com>
2023-03-06 18:41:28 +0530

    finished Today

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Alert from './components/Alert';
 import TextForm from "./components/TextForm";
 function App() {
   const [mode, setMode] = useState('light');
+  const[alert, setAlert] = useState(null) ;
+
+  
   const toggleMode = ()=>{
     if (mode === 'light') {
       setMode('dark') ;
@@ -19,6 +23,7 @@ function App() {
   return (
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert="This is alert"/>
       <div className="container">
         <TextForm heading="Enter the text to alalysis" />
       </div>
